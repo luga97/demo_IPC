81ede1a5751cc7b833dc266b5c137284c8fbb967
luga97 <ldgl1215@gmail.com>
2019-01-03 10:21:28 -0400

    Añadiendo archivos al repositorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace DOTNET_PLAY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bplay = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Tip = new System.Windows.Forms.TextBox();
+            this.Tport = new System.Windows.Forms.TextBox();
+            this.CBmainstream = new System.Windows.Forms.CheckBox();
+            this.Bstart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Bplay
+            // 
+            this.Bplay.Location = new System.Drawing.Point(520, 497);
+            this.Bplay.Name = "Bplay";
+            this.Bplay.Size = new System.Drawing.Size(75, 25);
+            this.Bplay.TabIndex = 0;
+            this.Bplay.Text = "Comenzar";
+            this.Bplay.UseVisualStyleBackColor = true;
+            this.Bplay.Click += new System.EventHandler(this.Bplay_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(645, 476);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BLogin
+            // 
+            this.BLogin.Location = new System.Drawing.Point(434, 496);
+            this.BLogin.Name = "BLogin";
+            this.BLogin.Size = new System.Drawing.Size(75, 25);
+            this.BLogin.TabIndex = 2;
+            this.BLogin.Text = "Login";
+            this.BLogin.UseVisualStyleBackColor = true;
+            this.BLogin.Click += new System.EventHandler(this.BLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 509);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 508);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "puerto";
+            // 
+            // Tip
+            // 
+            this.Tip.Location = new System.Drawing.Point(61, 503);
+            this.Tip.Name = "Tip";
+            this.Tip.Size = new System.Drawing.Size(124, 20);
+            this.Tip.TabIndex = 4;
+            this.Tip.Text = "192.168.0.253";
+            // 
+            // Tport
+            // 
+            this.Tport.Location = new System.Drawing.Point(258, 503);
+            this.Tport.Name = "Tport";
+            this.Tport.Size = new System.Drawing.Size(67, 20);
+            this.Tport.TabIndex = 4;
+            this.Tport.Text = "8091";
+            // 
+            // CBmainstream
+            // 
+            this.CBmainstream.AutoSize = true;
+            this.CBmainstream.Location = new System.Drawing.Point(368, 504);
+            this.CBmainstream.Name = "CBmainstream";
+            this.CBmainstream.Size = new System.Drawing.Size(62, 17);
+            this.CBmainstream.TabIndex = 5;
+            this.CBmainstream.Text = "主码流";
+            this.CBmainstream.UseVisualStyleBackColor = true;
+            // 
+            // Bstart
+            // 
+            this.Bstart.Location = new System.Drawing.Point(602, 496);
+            this.Bstart.Name = "Bstart";
+            this.Bstart.Size = new System.Drawing.Size(75, 25);
+            this.Bstart.TabIndex = 6;
+            this.Bstart.Text = "启动485";
+            this.Bstart.UseVisualStyleBackColor = true;
+            this.Bstart.Click += new System.EventHandler(this.Bstart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 536);
+            this.Controls.Add(this.Bstart);
+            this.Controls.Add(this.CBmainstream);
+            this.Controls.Add(this.Tport);
+            this.Controls.Add(this.Tip);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Bplay);
+            this.Name = "Form1";
+            this.Text = "Play video demo in C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Bplay;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Tip;
+        private System.Windows.Forms.TextBox Tport;
+        private System.Windows.Forms.CheckBox CBmainstream;
+        private System.Windows.Forms.Button Bstart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using NETSDKDLL_DOTNET;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace DOTNET_PLAY
+{
+    public partial class Form1 : Form
+    {
+
+        NETSDKDLL_DOTNET.fSerialDataCallBack fSeriaDataCallback = null;
+        NETSDKDLL_DOTNET.fRealDataCallBack fReadlCallBack = null;
+        NETSDKDLL_DOTNET.fDecCallBackFunction fDecodecall = null;
+        NETSDKDLL_DOTNET.StatusEventCallBack fStateEvent = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            NETSDKDLL_DOTNET.PLAYERDLL.IP_TPS_Init();
+            NETSDKDLL_DOTNET.NETSDKDLL.IP_NET_DVR_Init();
+            fDecodecall = new fDecCallBackFunction(OnDecCallBackFunction);
+            fReadlCallBack = new fRealDataCallBack(OnRealDataCallBack);
+            fStateEvent = new NETSDKDLL_DOTNET.StatusEventCallBack(OnStatusEventCallBack);
+            //fSeriaDataCallback = new fSerialDataCallBack();
+            NETSDKDLL.IP_NET_DVR_SetStatusEventCallBack(fStateEvent, IntPtr.Zero);
+        }
+
+
+
+
+        /// <summary>
+        /// 收到设备端传过来的485数据
+        /// </summary>
+        /// <param name="lUser"></param>
+        /// <param name="pRecvDataBuffer"></param>
+        /// <param name="dwBufSize"></param>
+        /// <param name="pUser"></param>
+        /// <returns></returns>
+        public int OnSeriaDataCallback(int lUser, IntPtr pRecvDataBuffer, int dwBufSize, IntPtr pUser)
+        {
+
+            return 0;
+        }
+
+        
+
+
+        public int lRealStreamId = 0;
+        public int nPortId = 0;
+
+
+        //NETSDKDLL_DOTNET.fSerialDataCallBack fSeriaDataCallback = null;
+
+
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            NETSDKDLL_DOTNET.NETSDKDLL.IP_NET_DVR_Cleanup();
+            NETSDKDLL_DOTNET.PLAYERDLL.IP_TPS_ReleaseAll();
+        }
+
+        public int OnRealDataCallBack(int lRealHandle, int dwDataType, IntPtr pBuffer, int dwBufSize, ref FRAME_EXTDATA pExtData)
+        {
+            IntPtr pWnd = pExtData.pUserData;
+            if (dwDataType == 0)
+            {
+                return PLAYERDLL.IP_TPS_InputVideoData(1, pBuffer, dwBufSize, pExtData.bIsKey, (int)pExtData.timestamp);
+            }
+            else if (dwDataType == 1)
+            {
+
+            }
+            else if (dwDataType == 2)
+            {
+                STREAM_AV_PARAM avParam = new STREAM_AV_PARAM();
+                avParam = (STREAM_AV_PARAM)Marshal.PtrToStructure(pBuffer, avParam.GetType());
+                int size = Marshal.SizeOf(typeof(VIDEO_PARAM));
+                IntPtr pVideoParam = Marshal.AllocHGlobal(10240);
+                Marshal.StructureToPtr(avParam.videoParam, pVideoParam, false);
+                NETSDKDLL_DOTNET.PLAYERDLL.IP_TPS_OpenStream(1, pVideoParam, size, 0, 40);
+                IntPtr p = new IntPtr(1);
+                NETSDKDLL_DOTNET.PLAYERDLL.IP_TPS_SetDecCallBack(1, fDecodecall,p);
+                Marshal.FreeHGlobal(pVideoParam);
+                if (avParam.bHaveAudio != 0)
+                {//说明有音频 
+                    int haveaudio = 1;
+                }
+                //NETSDKDLL_DOTNET.PLAYERDLL.IP_TPS_Play(1, pWnd);
+                NETSDKDLL_DOTNET.PLAYERDLL.IP_TPS_Play(1, IntPtr.Zero);
+            }
+            return 0;
+        }
+
+        public bool haveLogin = false;
+
+        public int OnStatusEventCallBack(int lUser, int nStateCode, IntPtr pResponse, IntPtr pUser)
+        {
+            if (nStateCode == (int)NETSDKDLL_DOTNET.enumNetSatateEvent.EVENT_LOGINOK)
+            {
+                haveLogin = true;
+            }
+            else if (nStateCode == (int)NETSDKDLL_DOTNET.enumNetSatateEvent.EVENT_LOGINFAILED || nStateCode == (int)NETSDKDLL_DOTNET.enumNetSatateEvent.EVENT_LOGIN_USERERROR)
+            {
+                haveLogin = false;
+            }
+            return 0;
+        }
+
+
+        public int OnDecCallBackFunction(int nPort, IntPtr pBuf, int nSize, ref FRAME_INFO pFrameInfo, IntPtr pUser, int nReserved2)
+        {
+            return 0;
+        }
+
+
+        private void Bplay_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (nLoginId == 0)
+            {
+                MessageBox.Show(this, "please login the device first!");
+                return;
+            }
+            else if (!haveLogin)
+            {
+                MessageBox.Show(this, "device online state error,please play video after login device!");
+                return;
+            }
+            if (lRealStreamId != 0)
+            {
+                NETSDKDLL_DOTNET.NETSDKDLL.IP_NET_DVR_StopRealPlay(lRealStreamId);
+            }
+            if (nPortId != 0)
+            {//如果当前正在播放，则停止解码
+                NETSDKDLL_DOTNET.PLAYERDLL.IP_TPS_CloseStream(nPortId);
+            }
+            NETSDKDLL_DOTNET.IP_NET_DVR_CLIENTINFO clientInfo = new IP_NET_DVR_CLIENTINFO();
+            NETSDKDLL_DOTNET.USRE_VIDEOINFO puser = new USRE_VIDEOINFO();
+            clientInfo.lChannel = 1;
+            puser.nVideoPort = 554;
+            if (CBmainstream.Checked)
+            {
+                puser.nVideoChannle = 0;//IPC设备的子码流
+            }
+            else
+            {
+                puser.nVideoChannle = 1;//IPC设备的子码流
+            }
+            puser.pUserData = pictureBox1.Handle;
+            nPortId = 1;
+            lRealStreamId = NETSDKDLL_DOTNET.NETSDKDLL.IP_NET_DVR_RealPlay(nLoginId, ref clientInfo, fReadlCallBack, ref puser, 1);
+        
+            if (lRealStreamId == 0)
+            {
+                MessageBox.Show(this, "please login the device first!");
+                return;
+            }
+
+           
+        }
+
+
+        private void BDecode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public int nLoginId = 0;
+        private void BLogin_Click(object sender, EventArgs e)
+        {
+            if (nLoginId != 0)
+            {
+                NETSDKDLL_DOTNET.NETSDKDLL.IP_NET_DVR_Logout(nLoginId);
+                haveLogin = false;
+            }
+            
+            NETSDKDLL_DOTNET.IP_NET_DVR_DEVICEINFO devInfo = new IP_NET_DVR_DEVICEINFO();
+            nLoginId = NETSDKDLL_DOTNET.NETSDKDLL.IP_NET_DVR_Login(Tip.Text, short.Parse(Tport.Text), "admin", "123456", ref devInfo);
+        }
+
+        private void Bstart_Click(object sender, EventArgs e)
+        {
+            if(nLoginId==0)
+            {
+                MessageBox.Show(this,"请先登录","");
+                return;
+            }
+            int nRet= NETSDKDLL_DOTNET.NETSDKDLL.IP_NET_DVR_SerialStart(nLoginId,fSeriaDataCallback,IntPtr.Zero);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
